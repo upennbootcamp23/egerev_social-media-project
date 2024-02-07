d999e76abad1da3db678f12b8a2ecdcd6aba81aa
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-07 11:31:29 -0500

    Added the MongoDB Node.js connection service

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let mongooseDB = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 let express = require('express');
 
 let application = express();
@@ -14,6 +15,27 @@ mongooseDB.connect(process.env.MONGODB_URI || "mongodb://localhost/socialmediaDB
     useUnifiedTopology: true,
     useNewUrlParser: true,
 });
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // log mongo queries being executed
 mongooseDB.set("debug", true);

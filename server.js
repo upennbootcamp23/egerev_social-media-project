cf42b92324cd707c75773d1ff9019109741b0d94
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-08 10:06:57 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 let mongooseDB = require("mongoose");
-const { MongoClient, ServerApiVersion } = require('mongodb');
 let express = require('express');
 
 let application = express();
@@ -9,35 +8,13 @@ application.use(express.json());
 application.use(express.urlencoded({ extended: true }));
 application.use(express.static('public'));
 
-mongooseDB.connect(process.env.MONGODB_URI || "mongodb://localhost/socialmediaDB", {
+mongooseDB.connect(process.env.MONGODB_URI || "mongodb://localhost/social-", {
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useNewUrlParser: true,
 });
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-
-    await client.connect();
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-// log mongo queries being executed
 mongooseDB.set("debug", true);
 
 application.use(require('./routes'));
